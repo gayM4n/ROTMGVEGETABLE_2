9b911df4ee07eeeefac6a263a5995a4ace45484f
gayM4n <iamjdogson@gmail.com>
2024-12-18 13:37:23 -0500

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Console.Out.WriteLine("I'm Gay");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
